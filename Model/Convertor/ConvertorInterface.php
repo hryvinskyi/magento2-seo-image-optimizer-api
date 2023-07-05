9d95b54ed0cf9e7f79669c9b0bf439c2766ef8ba
Vladymyr Hrivinskyi <volodymyr@hryvinskyi.com>
2023-07-06 02:14:25 +0300

    Update ConvertorInterface.php

--- Model/Convertor/ConvertorInterface.php
+++ Model/Convertor/ConvertorInterface.php
@@ -8,15 +8,43 @@ declare(strict_types=1);
 
 namespace Hryvinskyi\SeoImageOptimizerApi\Model\Convertor;
 
+/**
+ * Interface ConvertorInterface
+ *
+ * Represents the interface for optimize image.
+ */
 interface ConvertorInterface
 {
     /**
-     * Run command
+     * Returns the type of the image.
      *
-     * @param string $sourceImageUri
-     * @return void
+     * @return string The type of the image.
+     */
+    public function imageType(): string;
+
+    /**
+     * Check if convertor is enabled
      *
-     * @throws \Magento\Framework\Exception\FileSystemException
+     * @return bool
+     */
+    public function isEnabled(): bool;
+
+    /**
+     * Convert image given source URI.
+     *
+     * @param string $sourceImageUri The URI of the source image.
+     *
+     * @return string|null The processed image URI, or null if the method execution is unsuccessful.
      */
     public function execute(string $sourceImageUri): ?string;
+
+    /**
+     * Converts a image from input path to output path.
+     *
+     * @param string $inputPath The path of the input image.
+     * @param string $outputPath The path of the output image.
+     * 
+     * @return string If the conversion is successful return output path if not return input path.
+     */
+    public function convert(string $inputPath, string $outputPath): string;
 }
